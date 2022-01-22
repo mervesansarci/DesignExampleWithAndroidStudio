08b9c1f60c7a19f5e1532b2fad4591a5914c3545
Merve Sansarcı <mervesansarci@hotmail.com>
2022-01-22 03:30:56 +0300

    Initial commit

---
+++ app/src/main/java/com/denemeler/designexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.denemeler.designexample
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
